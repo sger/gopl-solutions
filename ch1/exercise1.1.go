3e5fb37a284082e22afbc79fb4fd124a727a8935
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-09 13:44:38 +0200

    updates

--- ch1/exercise1.1.go
+++ ch1/exercise1.1.go
@@ -6,13 +6,17 @@ import (
 	"strings"
 )
 
+// Modify the echo program to also print os.Args[0] the name of the command that invoked it.
 func main() {
 	s, sep := "", ""
+
 	for _, arg := range os.Args[0:] {
 		s += sep + arg
 		sep = " "
 	}
+
 	fmt.Println(s)
-	//sort syntax
+
+	// sort syntax
 	fmt.Println(strings.Join(os.Args[0:], " "))
 }

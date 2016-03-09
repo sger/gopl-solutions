55682dfa61bfb3a7a0baa1b87b2c8285915be443
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-09 13:46:22 +0200

    updates

--- ch1/exercise1.2.go
+++ ch1/exercise1.2.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// Modify the echo program to print the index and value of each of its arguments, one per line.
 func main() {
 	for index, arg := range os.Args[1:] {
 		fmt.Println(index, arg)
